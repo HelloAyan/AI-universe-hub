7d671dffac3a29e2d781983a3b3552d275b92e08
HelloAyan <helloayan171@gmail.com>
2023-03-04 21:13:35 +0600

    Display 6 items on home page using slice()

--- JS/dom.js
+++ JS/dom.js
@@ -10,6 +10,7 @@ const loadAlUniverse = async () => {
 
 const displayBlogs = blogs => {
     const blogContainer = document.getElementById('blogContainer');
+    blogs = blogs.slice(0, 6);
     blogs.forEach(blog => {
         const blogDiv = document.createElement('div');
         blogDiv.classList.add('col')
@@ -26,40 +27,36 @@ const displayBlogs = blogs => {
                     <button type="button" class="btn btn-danger rounded-3" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
                     <i class="fa-solid fa-arrow-right"></i>
                     </button> 
-                </p>
 
-                <!-- Modal -->
-                <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                <div class="modal-dialog modal-lg">
-                    <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modalBodyContainer">
-                        <div class="leftModal">
-                            <h5>${blog.description}</h5>
-                            <div class="subscriptionContainer">
-                                <div class="months">$10/ <br />month Basic</div>
-                                <div class="months">$50/ <br />month Basic</div>
-                                <div class="months">Contact us Enterprise</div>
+                    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+                    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-lg">
+                        <div class="modal-content">
+                            <div class="modal-header">
+    
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
+                            <div class="modalBodyContainer">
+                                <div class="leftModal">
+                                    <h5>${blog.description}</h5>
+                                    <div class="subscriptionContainer">
+                                        <div class="months">$10/ <br />month Basic</div>
+                                        <div class="months">$50/ <br />month Basic</div>
+                                        <div class="months">Contact us Enterprise</div>
+                                    </div>
+                                </div>
+                                <div class="rightModal">
+                                    <div> <img src=${blog.image} class="img-fluid"> </img> </div>
+                                    <h5 class="text-center m-2">Hi, how are you doing today?</h5>
+                                    <div class="text-center m-2">I'm doing well, thank you for asking. How can I assist you
+                                        today?</div>
+                                </div>
+                            </div>
+    
                         </div>
-                        <div class="rightModal">
-                            <div> <img src=${blog.image} class="img-fluid"> </img> </div>
-                            <h5 class="text-center m-2">Hi, how are you doing today?</h5>
-                            <div class="text-center m-2">I'm doing well, thank you for asking. How can I assist you today?</div>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-
                     </div>
                     </div>
-                </div>
-                </div>
-
-
+                </p>
                 <small class="text-muted">${blog.published_in}</small>
                 </div>
             </div>
