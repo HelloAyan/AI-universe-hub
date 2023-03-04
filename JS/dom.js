a60de19b2c5fc14bc9ff21c4885e99ed01c6b1c2
HelloAyan <helloayan171@gmail.com>
2023-03-04 18:16:14 +0600

    Added description part

--- JS/dom.js
+++ JS/dom.js
@@ -30,18 +30,26 @@ const displayBlogs = blogs => {
 
                 <!-- Modal -->
                 <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                <div class="modal-dialog">
+                <div class="modal-dialog modal-lg">
                     <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <div class="modal-body">
-                        ...
+                    <div class="modalBodyContainer">
+                        <div class="leftModal">
+                            <h5>${blog.description}</h5>
+                            <div class="subscriptionContainer">
+                                <div class="months">$10/ <br />month Basic</div>
+                                <div class="months">$50/ <br />month Basic</div>
+                                <div class="months">Contact us Enterprise</div>
+                            </div>
+                        </div>
+                        
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Understood</button>
+
                     </div>
                     </div>
                 </div>
